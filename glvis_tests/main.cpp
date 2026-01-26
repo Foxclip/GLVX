8d30440d2ff7a546355a0032e4022f34723f0c4a
Foxclip <destroyApp2@gmail.com>
2026-01-27 03:06:05 +1000

    Tests: vertex array modify test

--- glvis_tests/main.cpp
+++ glvis_tests/main.cpp
@@ -37,6 +37,7 @@ private:
     void viewRotateTest(test::Test& test);
     void vertexArrayTriangleTest(test::Test& test);
     void vertexArrayLineTest(test::Test& test);
+    void vertexArrayModifyTest(test::Test& test);
 };
 
 GlvisTestModule::GlvisTestModule(const std::string& name, test::TestModule* parent, const std::vector<test::TestNode*>& required_nodes)
@@ -57,6 +58,7 @@ GlvisTestModule::GlvisTestModule(const std::string& name, test::TestModule* pare
     auto view_rotate_test = addTest("view_rotate", { rectangle_test }, [&](test::Test& test) { viewRotateTest(test); });
     auto vertex_array_triangle_test = addTest("vertex_array_triangle", { clear_test }, [&](test::Test& test) { vertexArrayTriangleTest(test); });
     auto vertex_array_line_test = addTest("vertex_array_line", { clear_test }, [&](test::Test& test) { vertexArrayLineTest(test); });
+    auto vertex_array_modify_test = addTest("vertex_array_modify", { vertex_array_triangle_test }, [&](test::Test& test) { vertexArrayModifyTest(test); });
 }
 
 bool GlvisTestModule::checkPixelColor(test::Test& test, const Image& image, int startX, int startY, int endX, int endY, const Color& expectedColor) {
@@ -697,13 +699,79 @@ void GlvisTestModule::vertexArrayLineTest(test::Test& test) {
    T_COMPARE(image.getPixel(below_line), Color::Black, &Color::toString);
 }
 
+void GlvisTestModule::vertexArrayModifyTest(test::Test& test) {
+    const Vector2i window_size = Vector2i(100, 100);
+    window.setSize(window_size);
+    window.setTitle("vertex array triangle");
+    View view;
+    Vector2f window_center = window.getCenter();
+    view.setPosition(window_center);
+    window.setView(view);
+    window.clear(Color::Black);
+
+    // Render initial triangle
+    const int initial_vertex_count = 3;
+    VertexArray vertex_array(PrimitiveType::Triangles, initial_vertex_count);
+    const Vector2f triangle_base_left = Vector2f(0, 0);
+    const Vector2f triangle_base_right = Vector2f(10, 0);
+    const Vector2f triangle_top = Vector2f(5, 10);
+    vertex_array[0] = Vertex(triangle_base_left, Color::Red, Vector2f(0, 0));
+    vertex_array[1] = Vertex(triangle_base_right, Color::Red, Vector2f(0, 0));
+    vertex_array[2] = Vertex(triangle_top, Color::Red, Vector2f(0, 0));
+    vertex_array.syncBuffer();
+    window.draw(vertex_array);
+    window.display();
+
+    // Check initial pixels
+    Image initial_image = window.readPixels();
+    const Vector2i triangle_center_check = Vector2i(5, 5);
+    const Vector2i triangle_left_check = Vector2i(2, 2);
+    const Vector2i outside_initial = Vector2i(15, 5);
+    T_COMPARE(initial_image.getPixel(triangle_center_check), Color::Red, &Color::toString);
+    T_COMPARE(initial_image.getPixel(triangle_left_check), Color::Red, &Color::toString);
+    T_COMPARE(initial_image.getPixel(outside_initial), Color::Black, &Color::toString);
+
+    // Add additional vertices to the VertexArray
+    const Vector2f second_triangle_base_left = Vector2f(20, 0);
+    const Vector2f second_triangle_base_right = Vector2f(30, 0);
+    const Vector2f second_triangle_top = Vector2f(25, 10);
+    vertex_array.append(Vertex(second_triangle_base_left, Color::Red, Vector2f(0, 0)));
+    vertex_array.append(Vertex(second_triangle_base_right, Color::Red, Vector2f(0, 0)));
+    vertex_array.append(Vertex(second_triangle_top, Color::Red, Vector2f(0, 0)));
+    vertex_array.syncBuffer();
+    window.clear(Color::Black);
+    window.draw(vertex_array);
+    window.display();
+
+    // Check updated pixels
+    Image updated_image = window.readPixels();
+    const Vector2i second_triangle_center_check = Vector2i(25, 5);
+    const Vector2i second_triangle_left_check = Vector2i(22, 2);
+    T_COMPARE(initial_image.getPixel(second_triangle_center_check), Color::Black, &Color::toString);
+    T_COMPARE(initial_image.getPixel(second_triangle_left_check), Color::Black, &Color::toString);
+    T_COMPARE(updated_image.getPixel(triangle_center_check), Color::Red, &Color::toString); // Still red
+    T_COMPARE(updated_image.getPixel(second_triangle_center_check), Color::Red, &Color::toString); // New area
+    T_COMPARE(updated_image.getPixel(second_triangle_left_check), Color::Red, &Color::toString); // New area
+    T_COMPARE(updated_image.getPixel(outside_initial), Color::Black, &Color::toString); // Still black
+
+    // Delete the added vertices (removing the second triangle)
+    vertex_array.resize(3);
+    vertex_array.syncBuffer();
+    window.clear(Color::Black);
+    window.draw(vertex_array);
+    window.display();
+
+    // Check final image matches initial
+    Image final_image = window.readPixels();
+    T_WRAP_CONTAINER(compareImages(test, final_image, initial_image));
+}
+
 int main() {
     test::TestModule root("glvis tests", nullptr);
     GlvisTestModule* glvisModule = root.addModule<GlvisTestModule>("Basic");
     root.run();
     root.printSummary();
 
-    // TODO: put each shader in shaders/ folder into a single file
     // TODO: shader tests
     // TODO: make VertexArray upload data on every render and remove syncBuffer
     // TODO: text rendering
