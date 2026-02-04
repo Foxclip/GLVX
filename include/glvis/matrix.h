0d5a35c46249f25ea3acb97f734c4b7897be842b
Foxclip <destroyApp2@gmail.com>
2026-02-05 02:08:20 +1000

    Matrix: add get and toString methods

--- include/glvis/matrix.h
+++ include/glvis/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <string>
 #include "glvis/vector.h"
 
 namespace glvis {
@@ -11,8 +12,9 @@ public:
     Matrix4(const std::array<float, 16>& data);
     Matrix4(const float* data);
     const float* getData() const;
+    float get(int row, int col) const;
+    std::string toString() const;
     Matrix4 operator*(const Matrix4& other) const;
-
     static Matrix4 translate(const Matrix4& matrix, const Vector3& v);
 
 private:
