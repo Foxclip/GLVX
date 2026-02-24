a15ff02b54eac463ce9dd1c998cbc31cf3d8bef0
Foxclip <destroyApp2@gmail.com>
2026-02-24 23:01:00 +1000

    Font: lazily evaluate FreeType error messages

--- src/font.cpp
+++ src/font.cpp
@@ -6,12 +6,12 @@ namespace glvis {
 bool Font::is_library_initialized = false;
 FT_Library Font::library = nullptr;
 
-#define FREETYPE_CALL(func, message) \
+#define FREETYPE_CALL(func, messageGetter) \
     { \
         FT_Error error = func; \
         if (error != 0) { \
             throw std::runtime_error( \
-                std::string(message) \
+                std::string(messageGetter()) \
                 + ", error code: " \
                 + std::to_string(error) \
             ); \
@@ -35,25 +35,25 @@ void Font::openFromFile(const std::filesystem::path& filename) {
 
 void Font::loadFont(const std::filesystem::path& filename) {
     if (!is_library_initialized) {
-        FREETYPE_CALL(FT_Init_FreeType(&library), "Failed to initialize FreeType library");
+        FREETYPE_CALL(FT_Init_FreeType(&library), []() { return "Failed to initialize FreeType library"; });
         is_library_initialized = true;
     }
     if (face) {
-        FREETYPE_CALL(FT_Done_Face(face), "Failed to unload font");
+        FREETYPE_CALL(FT_Done_Face(face), []() { return "Failed to unload font"; });
     }
     characters.clear();
 
     std::string filename_str = filename.string();
     const char* filename_cstr = filename_str.c_str();
-    FREETYPE_CALL(FT_New_Face(library, filename_cstr, 0, &face), "Failed to load font file");
-    FREETYPE_CALL(FT_Set_Pixel_Sizes(face, 0, 48), "Failed to set font size");
+    FREETYPE_CALL(FT_New_Face(library, filename_cstr, 0, &face), []() { return "Failed to load font file"; });
+    FREETYPE_CALL(FT_Set_Pixel_Sizes(face, 0, 48), []() { return "Failed to set font size"; });
     GL_CALL(glPixelStorei(GL_UNPACK_ALIGNMENT, 1));
     for (unsigned char c = 32; c < 127; c++) {
         FREETYPE_CALL(
             FT_Load_Char(face, c, FT_LOAD_RENDER),
-            "Failed to load character: "
-                + std::to_string(c)
-                + " ( " + std::string(1, c) + ")"
+            [&]() {
+                return "Failed to load character: " + std::to_string(c) + " (" + std::string(1, c) + ")";
+            }
         );
         unsigned char* buffer = face->glyph->bitmap.buffer;
         unsigned int width = face->glyph->bitmap.width;
