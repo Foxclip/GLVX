4a91e19278d8eaa4ffd23d44bb95d712f9f2e965
Foxclip <destroyApp2@gmail.com>
2026-07-07 23:49:45 +1000

    UniformBuffer: replace hardcoded size constants with sizeof

--- src/uniform_buffer.cpp
+++ src/uniform_buffer.cpp
@@ -19,34 +19,35 @@ UniformBuffer::~UniformBuffer() {
 void UniformBuffer::createCameraUBO() {
     GL_CALL(glGenBuffers(1, &cameraID));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, cameraID));
-    GL_CALL(glBufferData(GL_UNIFORM_BUFFER, 256, nullptr, GL_DYNAMIC_DRAW));
+    GL_CALL(glBufferData(GL_UNIFORM_BUFFER, 2 * sizeof(Matrix4), nullptr, GL_DYNAMIC_DRAW));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, 0));
 }
 
 void UniformBuffer::createObjectUBO() {
     GL_CALL(glGenBuffers(1, &objectID));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, objectID));
-    GL_CALL(glBufferData(GL_UNIFORM_BUFFER, 256, nullptr, GL_DYNAMIC_DRAW));
+    GL_CALL(glBufferData(GL_UNIFORM_BUFFER, sizeof(Matrix4) + 5 * sizeof(float), nullptr, GL_DYNAMIC_DRAW));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, 0));
 }
 
 void UniformBuffer::updateCameraUBO(const Matrix4& view, const Matrix4& projection) {
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, cameraID));
-    GL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 0, 64, view.getData()));
-    GL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 64, 64, projection.getData()));
+    GL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(Matrix4), view.getData()));
+    GL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, sizeof(Matrix4), sizeof(Matrix4), projection.getData()));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, 0));
 }
 
 void UniformBuffer::updateObjectUBO(const Matrix4& model, const Color& color, bool hasTexture) {
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, objectID));
 
-    float data[21] = {};
-    std::memcpy(data, model.getData(), 64);
-    data[16] = color.r;
-    data[17] = color.g;
-    data[18] = color.b;
-    data[19] = color.a;
-    data[20] = hasTexture ? 1.0f : 0.0f;
+    float data[sizeof(Matrix4) / sizeof(float) + 5] = {};
+    std::memcpy(data, model.getData(), sizeof(Matrix4));
+    size_t offset = sizeof(Matrix4) / sizeof(float);
+    data[offset + 0] = color.r;
+    data[offset + 1] = color.g;
+    data[offset + 2] = color.b;
+    data[offset + 3] = color.a;
+    data[offset + 4] = hasTexture ? 1.0f : 0.0f;
 
     GL_CALL(glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(data), data));
     GL_CALL(glBindBuffer(GL_UNIFORM_BUFFER, 0));
