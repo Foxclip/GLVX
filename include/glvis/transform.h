2abdbc525680dbfaace3e159e30a0714574dc847
Foxclip <destroyApp2@gmail.com>
2026-07-28 02:50:20 +1000

    Transform: remove redundant spaces

--- include/glvis/transform.h
+++ include/glvis/transform.h
@@ -13,28 +13,19 @@ public:
 
     Transform& translate(float x, float y);
     Transform& translate(const Vector2f& vector);
-
     Transform& rotate(float angle, const Vector2f& center);
-
     Transform& scale(float factor_x, float factor_y, float center_x, float center_y);
     Transform& scale(float factor_x, float factor_y);
     Transform& scale(const Vector2f& factor, const Vector2f& center);
     Transform& scale(const Vector2f& factor);
-
     Transform& combine(const Transform& transform);
-
     Vector2f transformPoint(float x, float y) const;
     Vector2f transformPoint(const Vector2f& point) const;
-
     FloatRect transformRect(const FloatRect& rect) const;
-
     Transform getInverse() const;
-
     Matrix4 toMatrix4() const;
-
     Transform& operator*=(const Transform& transform);
     Vector2f operator*(const Vector2f& point) const;
-
     bool operator==(const Transform& other) const;
     bool operator!=(const Transform& other) const;
 
