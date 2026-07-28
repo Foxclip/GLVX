015792723680a94ddfaa4a7c79b5b9d10021f50b
Foxclip <destroyApp2@gmail.com>
2026-07-28 18:54:36 +1000

    Cursor: minor refactor in loadFromSystem

--- src/cursor.cpp
+++ src/cursor.cpp
@@ -2,8 +2,7 @@
 
 namespace glvis {
 
-Cursor::Cursor() {
-}
+Cursor::Cursor() { }
 
 Cursor::~Cursor() {
     if (glfw_cursor) {
@@ -14,33 +13,15 @@ Cursor::~Cursor() {
 bool Cursor::loadFromSystem(Type type) {
     int glfw_type = 0;
     switch (type) {
-        case Type::Arrow:
-            glfw_type = GLFW_ARROW_CURSOR;
-            break;
-        case Type::IBeam:
-            glfw_type = GLFW_IBEAM_CURSOR;
-            break;
-        case Type::Crosshair:
-            glfw_type = GLFW_CROSSHAIR_CURSOR;
-            break;
-        case Type::Hand:
-            glfw_type = GLFW_HAND_CURSOR;
-            break;
-        case Type::ResizeHorizontal:
-            glfw_type = GLFW_RESIZE_EW_CURSOR;
-            break;
-        case Type::ResizeVertical:
-            glfw_type = GLFW_RESIZE_NS_CURSOR;
-            break;
-        case Type::ResizeDiagonal1:
-            glfw_type = GLFW_RESIZE_NWSE_CURSOR;
-            break;
-        case Type::ResizeDiagonal2:
-            glfw_type = GLFW_RESIZE_NESW_CURSOR;
-            break;
-        case Type::ResizeAll:
-            glfw_type = GLFW_RESIZE_ALL_CURSOR;
-            break;
+        case Type::Arrow:            glfw_type = GLFW_ARROW_CURSOR;       break;
+        case Type::IBeam:            glfw_type = GLFW_IBEAM_CURSOR;       break;
+        case Type::Crosshair:        glfw_type = GLFW_CROSSHAIR_CURSOR;   break;
+        case Type::Hand:             glfw_type = GLFW_HAND_CURSOR;        break;
+        case Type::ResizeHorizontal: glfw_type = GLFW_RESIZE_EW_CURSOR;   break;
+        case Type::ResizeVertical:   glfw_type = GLFW_RESIZE_NS_CURSOR;   break;
+        case Type::ResizeDiagonal1:  glfw_type = GLFW_RESIZE_NWSE_CURSOR; break;
+        case Type::ResizeDiagonal2:  glfw_type = GLFW_RESIZE_NESW_CURSOR; break;
+        case Type::ResizeAll:        glfw_type = GLFW_RESIZE_ALL_CURSOR;  break;
     }
 
     if (glfw_cursor) {
