5d1e57773f741461b9ca539a1e8e0e10a34aeef5
Foxclip <destroyApp2@gmail.com>
2026-08-04 17:49:52 +1000

    Keyboard: make setKeyState private

--- include/glvis/keyboard.h
+++ include/glvis/keyboard.h
@@ -8,10 +8,13 @@ class Keyboard {
 public:
     static bool isKeyPressed(Key key);
     static bool isModifierActive(Modifier modifier);
-    static void setKeyState(Key key, bool pressed);
     static void reset();
 
 private:
+    friend class Window;
+
+    static void setKeyState(Key key, bool pressed);
+
     static constexpr int NumKeys = 350;
     static bool key_states[NumKeys];
 };
